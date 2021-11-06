2b9908cb2b12041042b0f2d4ef970f37c7d7d6f1
mrpekr <49102192+mrpekr@users.noreply.github.com>
2021-11-06 11:30:28 +0100

    Přidáne Jmenování + Trochu překladu
    * Nový Systém jména (Lze Zapnout:  "customsalution = True")
    * Přeložené Komentáře Codu
    
    * Co Přidat:
    Možnost Změnit Jméno (Zatím moužné jen smazáním souboru "data")

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import random
 from datetime import datetime
 from pypresence import Presence
 import time
+from os.path import exists
+import sys
+import os
 
 clearConsole = lambda: print('\n' * 150)
 
 #Config:
-richpresence = True #Default: True (Shows That you are using "The Gengleman" as you discord activity)
-customsalutation = False#COMING SOON | Default: False ("The Gentleman" will be calling you by custom name)
+richpresence = True #Výchozý: True (Ukazuje že používáš "Genglemanana" jako aktivitu na Discordu)
+customsalutation = False #Výchozý: False ("Gentleman" tě bude nazívat tvým jménem)
 
 def rpc():
     client_id = "906113842969460746"
@@ -125,4 +128,133 @@ def Menu():
         clearConsole()
         print("Neplatná Volba")
         print(Menu())
-Menu()
+
+if customsalutation == False:
+    Menu()
+else:
+    file_exists = exists("data")
+    if file_exists == True:
+            f = open("data","r")
+            lines = f.readline()
+            name = lines
+            def MenuN():
+                if richpresence == True:
+                    rpc()
+                print("\x1b[38;5;220m")    
+                print("==============================================================================================")
+                print("=        ==  ==================      =======================  ================================")
+                print("====  =====  =================   ==   ======================  ================================")
+                print("====  =====  =================  ====  =================  ===  ================================")
+                print("====  =====  ======   ========  =========   ===  = ===    ==  ===   ===  =  = ====   ===  = ==")
+                print("====  =====    ===  =  =======  ========  =  ==     ===  ===  ==  =  ==        ==  =  ==     =")
+                print("====  =====  =  ==     =======  ===   ==     ==  =  ===  ===  ==     ==  =  =  =====  ==  =  =")
+                print("====  =====  =  ==  ==========  ====  ==  =====  =  ===  ===  ==  =====  =  =  ===    ==  =  =")
+                print("====  =====  =  ==  =  =======   ==   ==  =  ==  =  ===  ===  ==  =  ==  =  =  ==  =  ==  =  =")
+                print("====  =====  =  ===   =========      ====   ===  =  ===   ==  ===   ===  =  =  ===    ==  =  =")
+                print("==============================================================================================")
+                print(" ")
+                print('Vytvořil: Mr. Pekr | https://mrpekr.github.io ')
+                print("\x1b[38;5;255m")
+                
+                print("Co pro tebe můžu udělat " + name + "?")
+                print("\x1b[38;5;199m[1] Řekni mi vtip (Bez Překladu)")
+                print("[2] Řekni mi mou budoucnost (Bez Překladu)")
+                print("[3] Řekni mi kolik je hodin")
+                print("[4] Tady Nic Není")
+                menuN = input("\x1b[38;5;85mZadej Čáslo: ")
+                print("\x1b[38;5;255m")
+                if menuN == "1":
+                    def Joke():
+
+                        clearConsole()
+                        jokes = [
+                            "Did you hear about the mathematician who’s afraid of negative numbers? He’ll stop at nothing to avoid them.",
+                            "Why do we tell actors to “break a leg?” Because every play has a cast.",
+                            "Heard about the new restaurant called Karma? There’s no menu: You get what you deserve.",
+                            "Did you hear about the actor who fell through the floorboards? He was just going through a stage.",
+                            "Did you hear about the claustrophobic astronaut? He just needed a little space.",
+                            "Why don’t scientists trust atoms? Because they make up everything.",
+                            "How do you drown a hipster? Throw him in the mainstream.",
+                            "What did the Buddhist say to the hot dog vendor? Make me one with everything."
+                        ]
+                        print("Tady to máš : \x1b[38;5;158m" + random.choice(jokes))
+                        print("\x1b[38;5;15m")
+                        joke = input("Chceš Další Vtip? [Ano / Ne]: ")
+                        if joke == "Ano":
+                            clearConsole()
+                            print(Joke())
+                        elif joke == "Ne":
+                            clearConsole()
+                            print(MenuN())
+                        else:
+                            clearConsole() 
+                            print("Neplatná Volba")
+                            Joke()          
+                    Joke()
+                elif menuN == "2":
+                    def Future():
+                        clearConsole()
+                        futures = [
+                            "You Will Be Rich With a lot of Woman/Man on your side with a LOT of money in your wallet",
+                            "You became Poor and lost everything. How Sad ....",
+                            "You Died in an Acident ... Sorry i can't see what happend only you in Hospital ... Dieing",
+                            "You Have Kid and a family you are finnaly happy. (if you already have kid/s turn me off and take care of them)",
+                            "Sorry ... My powers don't see anything",
+                            "I took ever your body and took over the world. Haha I was acting ... or Was I?"
+                        ]
+                        print("My Powers Tell Me That ...: \x1b[38;5;158m" + random.choice(futures))
+                        print("\x1b[38;5;15m")
+                        future = input("Do You Want To Try Again? [Ano / Ne]: ")
+
+                        if future == "Ano":
+                            clearConsole()
+                            print(Future())
+
+                        if future == "Ne":
+                            clearConsole()
+                            print(MenuN())
+                    Future()
+                elif menuN == "3":
+                    def Time():
+                        print("Aktuálně je: " + datetime.now().strftime('%H:%M:%S'))
+                        back = input("Cheš zpátky do Menu? [Ano / Ne]: ")
+                        if back == "Ano":
+                            clearConsole()
+                            print(MenuN())
+
+                        if back == "Ne":
+                            clearConsole()
+                            print(Time())
+                    Time()
+                elif menuN == "4":
+                    def placeholder():
+                        print("You Have Selected A Place Holder")
+                        print("You Stupid")
+                        back = input("Return To Menu? [Ano / Ne]: ")
+                        if back == "Ano":
+                            clearConsole()
+                            print(MenuN())
+                        elif back == "Ne":
+                            clearConsole()
+                            print(placeholder())
+                        else:
+                            clearConsole()
+                            print("Neplatná Volba")
+                            print(placeholder())
+                    placeholder()
+                else:
+                    clearConsole()
+                    print("Neplatná Volba")
+                    print(Menu())
+            MenuN()
+    else:
+        print("Ahoj Já Jsem Gentleman. Tebe Neznám... Budu potřebovat tvoje jméno")
+        time.sleep(5)
+        name = input("Zadej Prosím Své Jméno: ")
+        with open("data", "w") as f:
+            f.write(name)
+            f = open("data","r")
+            lines = f.readline()
+            names = lines
+            print("Jméno Uloženo.")
+        os.execv(sys.executable, ['python'] + sys.argv)
